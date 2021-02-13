92b56517e3cbe024133f0d51b89a2e9a1812153d
sunnychung98 <59641147+sunnychung98@users.noreply.github.com>
2021-02-13 17:47:45 +0900

    loginpage update

--- src/main/java/kr/code/homes/controller/LoginController.java
+++ src/main/java/kr/code/homes/controller/LoginController.java
@@ -8,7 +8,7 @@ public class LoginController {
 
     @RequestMapping("/login")
     public String login(){
-        return "views/login/loginForm";
+        return "views/contents/loginForm";
     }
 
 }
