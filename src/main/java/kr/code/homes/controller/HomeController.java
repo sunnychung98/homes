0df9e621d3ac6b28fe13b37fb51519361699a5ec
sunnychung98 <59641147+sunnychung98@users.noreply.github.com>
2021-02-15 19:13:09 +0900

    loginpage design update

--- src/main/java/kr/code/homes/controller/HomeController.java
+++ src/main/java/kr/code/homes/controller/HomeController.java
@@ -1,41 +1,18 @@
 package kr.code.homes.controller;
 
 import kr.code.homes.service.TestService;
-import kr.code.homes.vo.TestVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Controller
 public class HomeController {
 
-    private TestService service;
-
-    @Autowired
-    public void setTestService(TestService service){
-        this.service=service;
-    }
-
-    @RequestMapping("/main/view")
+    @RequestMapping("/")
     public ModelAndView main(@RequestParam(value="id", required=false) String userId) {
         ModelAndView mav = new ModelAndView();
-        try {
-            Map<String, Object> param = new HashMap<String, Object>();
-            List<TestVO> list = service.getList(param);
-
-            mav.addObject("list", list);
-            mav.addObject("size", list == null ?  0 : list.size());
-            mav.addObject("pageTitle", "Welcome Home!");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
 
         mav.setViewName("views/contents/main");
 
--- src/main/java/kr/code/homes/controller/LoginController.java
+++ src/main/java/kr/code/homes/controller/LoginController.java
@@ -1,14 +1,53 @@
 package kr.code.homes.controller;
 
+import kr.code.homes.service.LoginService;
+import kr.code.homes.service.TestService;
+import kr.code.homes.vo.MemberVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.SQLException;
 
 @Controller
+@RequestMapping("/account")
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String login(){
-        return "views/contents/login";
+    private LoginService service;
+
+    @Autowired
+    public void setLoginService(LoginService service){
+        this.service=service;
     }
+
+    @GetMapping("/login")
+    public ModelAndView login(){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("pageTitle", "로그인하세요");
+        mav.setViewName("views/contents/login");
+        mav.addObject("pageName", "login");
+        return mav;
+    }
+
+    @RequestMapping(value="/loginOk", method=RequestMethod.POST)
+    public ModelAndView loginOk(MemberVO vo) throws SQLException {
+        ModelAndView mav = new ModelAndView();
+        MemberVO resultVO = service.loginCheck();
+        if(resultVO == null){
+            mav.setViewName("/views/contents/login");
+            System.out.println("로그인실패");
+        }else{
+            mav.addObject("LoginName", resultVO.getUsername());
+            mav.setViewName("/views/contents/main");
+            System.out.println("로그인성공");
+        }
+        return mav;
+    }
+
+
 
 }
